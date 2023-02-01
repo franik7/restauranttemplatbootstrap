95c6be399bc700cf6bd7bc87e016386e1c164bc0
Frants <118231497+franik7@users.noreply.github.com>
2023-01-31 22:24:30 -0500

    Add files via upload

---
+++ dh.php
@@ -0,0 +1,16 @@
+<?php
+
+$d = file_get_contents('data.json');
+$d = json_decode($d, true);
+array_unshift($d, $_POST);
+$d = json_encode($d);
+
+
+
+
+file_put_contents('data.json', $d);
+
+
+header( 'location:../index.html' );
+
+?>
